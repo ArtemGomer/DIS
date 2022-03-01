fa25b6f1f15d54df3a2bbea0af098101a652ab20
Гомер Артем Константинович <happycrown30062000@gmail.com>
2022-03-01 14:38:39 +0700

    lab2 final

--- ris_lab_1/src/main/java/Main.java
+++ ris_lab_1/src/main/java/Main.java
@@ -1,24 +1,16 @@
-import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        try (
-                InputStream fileInputStream = Main.class.getClassLoader().getResourceAsStream("RU-NVS.osm.bz2");
-                InputStream bZip2CompressorInputStream = new BZip2CompressorInputStream(fileInputStream)
-        ) {
+        try {
             logger.info("Create parser");
             OsmParser parser = new OsmParser();
             logger.info("Try to parse stream");
-            parser.parseStream(bZip2CompressorInputStream);
-            OsmResult result = parser.getResult();
+            OsmResult result = parser.parseStream("RU-NVS.osm.bz2");
             logger.info("Get parse result");
             result.printUserToChanges();
             result.printKeyToNodes();
--- ris_lab_1/src/main/java/OsmParser.java
+++ ris_lab_1/src/main/java/OsmParser.java
@@ -1,85 +1,51 @@
+import generated.Osm;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
-import javax.xml.namespace.QName;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.sax.SAXSource;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class OsmParser {
 
-    private final Logger logger = LogManager.getLogger(OsmParser.class);
+    private final static Logger logger = LogManager.getLogger(OsmParser.class);
 
-    private final String NODE_TAG = "node";
-    private final String TAG_TAG = "tag";
+    private final Unmarshaller unmarshaller;
 
-    private final QName USER_ATTR = new QName("user");
-    private final QName K_ATTR = new QName("k");
-
-    private OsmResult result = null;
-
-    private XMLEventReader reader = null;
-
-    public void parseStream(InputStream stream) throws XMLStreamException {
-        result = new OsmResult();
-        logger.info("Try to create xml reader");
-        reader = createReader(stream);
-        logger.info("Xml reader created");
-        logger.info("Try to iterate nodes");
-        iterateNodes();
-        reader.close();
-        logger.info("Xml reader closed");
+    public OsmParser() throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(Osm.class);
+        unmarshaller = context.createUnmarshaller();
     }
 
-    public OsmResult getResult() {
+    public OsmResult parseStream(String fileName) throws IOException, JAXBException, SAXException {
+        InputStream fileInputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+        BZip2CompressorInputStream inputStream = new BZip2CompressorInputStream(fileInputStream);
+        XMLReader reader = XMLReaderFactory.createXMLReader();
+        NamespaceFilter filter = new NamespaceFilter("http://openstreetmap.org/osm/0.6", true);
+        filter.setParent(reader);
+        InputSource source = new InputSource(inputStream);
+        SAXSource saxSource = new SAXSource(filter, source);
+        logger.info("Try to unmarshall osm");
+        Osm osm = (Osm) unmarshaller.unmarshal(saxSource);
+        logger.info("Process result");
+        return processOsm(osm);
+    }
+
+    private OsmResult processOsm(Osm osm) {
+        OsmResult result = new OsmResult();
+        osm.getNode().forEach(node -> {
+            result.addChangeToUser(node.getUser());
+            node.getTag().forEach(tag -> result.addNodeToKey(tag.getK()));
+        });
         return result;
-    }
-
-    private void iterateNodes() throws XMLStreamException {
-        while (reader.hasNext()) {
-            XMLEvent event = reader.nextEvent();
-            if (event.isStartElement()) {
-                StartElement startElement = event.asStartElement();
-                if (startElement.getName().getLocalPart().equals(NODE_TAG)) {
-                    processNode(startElement);
-                }
-            }
-        }
-        logger.info("All nodes are iterated");
-    }
-
-    private void processNode(StartElement startElement) throws XMLStreamException {
-        String userName = startElement.getAttributeByName(USER_ATTR).getValue();
-        result.addChangeToUser(userName);
-        processNodeKeys();
-    }
-
-    private void processNodeKeys() throws XMLStreamException {
-        while (reader.hasNext()) {
-            XMLEvent event = reader.nextEvent();
-            if (event.isEndElement()) {
-                EndElement endElement = event.asEndElement();
-                if (endElement.getName().getLocalPart().equals(NODE_TAG)) {
-                    return;
-                }
-            }
-            if (event.isStartElement()) {
-                StartElement startElement = event.asStartElement();
-                if (startElement.getName().getLocalPart().equals(TAG_TAG)) {
-                    String key = startElement.getAttributeByName(K_ATTR).getValue();
-                    result.addNodeToKey(key);
-                }
-            }
-        }
-    }
-
-    private XMLEventReader createReader(InputStream stream) throws XMLStreamException {
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        return xmlInputFactory.createXMLEventReader(stream);
     }
 
 }
